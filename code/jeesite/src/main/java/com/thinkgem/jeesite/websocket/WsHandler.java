bf188ba77c2ccf0abf7c3f5118182e9de5d872d2
liuji789 <583115762@qq.com>
2019-11-20 22:26:30 +0800

    websocket

---
+++ code/jeesite/src/main/java/com/thinkgem/jeesite/websocket/WsHandler.java
@@ -0,0 +1,67 @@
+package com.thinkgem.jeesite.websocket;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class WsHandler extends TextWebSocketHandler {
+
+
+    private final static List<WebSocketSession> users;
+
+    static {
+        users = new CopyOnWriteArrayList<WebSocketSession>();
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        //建立连接后的操作
+        users.add(session);
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        //收到消息后的操作
+        super.handleMessage(session, message);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        users.remove(session);
+        //连接出错后的操作
+        super.handleTransportError(session, exception);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        //连接关闭后的操作
+        users.remove(session);
+        super.afterConnectionClosed(session, closeStatus);
+    }
+
+    /**
+     * 给所有在线用户发送消息
+     *
+     * @param message
+     */
+    public void sendMessageToUsers(TextMessage message) {
+        for (WebSocketSession user : users) {
+            try {
+                if (user.isOpen()) {
+                    user.sendMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
